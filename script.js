ca333ecf0fe19c3a558fd5c531641bfb357f399f
Blair <120442257+cocos-beans@users.noreply.github.com>
2023-11-02 21:33:27 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function github() {
+  window.open("https://github.com/cocos-beans", '_blank')
+}
+
+function twitter() {
+  window.open("https://twitter.com/cocosbeans", '_blank')
+}
+
+function steam() {
+  window.open("https://steamcommunity.com/id/six_eighteen_fourty_five/", '_blank')
+}
+
+function dev() {
+  window.open("https://dev.cocosbeans.me")
+}
