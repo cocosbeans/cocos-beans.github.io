8cf9267bcd153a709996b09115046a068f6ba912
Blair <120442257+cocos-beans@users.noreply.github.com>
2023-11-04 12:00:24 -0500

    Create script.js

---
+++ history/nov2-1/script.js
@@ -0,0 +1,15 @@
+function github() {
+  window.open("https://github.com/cocos-beans", '_blank')
+}
+
+function twitter() {
+  window.open("https://twitter.com/cocosbeans", '_blank')
+}
+
+function steam() {
+  alert('This alert window is not part of this history, however you cannot view this.')
+}
+
+function dev() {
+  window.open("https://dev.cocosbeans.me")
+}
